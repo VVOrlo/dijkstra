d74d7a602ee7e17579dd117abc1fa565f66e52e1
VVOrlov <vovkaorlov@gmail.com>
2024-05-04 17:34:47 +0700

    -

--- main.py
+++ main.py
@@ -4,45 +4,45 @@ from math import inf
 
 def dijkstra(graph, start_node):
     # Создаем словарь для хранения расстояний
-    distances = {}
-    for node in graph.nodes():
-        distances[node] = inf
-    distances[start_node] = 0
+    dist = {}
+    for n in graph.nodes():
+        dist[n] = inf
+    dist[start_node] = 0
 
     # Создаем словарь для хранения предшественников
-    predecessors = {}
-    for node in graph.nodes():
-        predecessors[node] = None
+    preds = {}
+    for n in graph.nodes():
+        preds[n] = None
 
     # Создаем список непосещенных вершин
-    unvisited_nodes = list(graph.nodes())
+    unvisited = list(graph.nodes())
 
-    while unvisited_nodes:
+    while unvisited:
         # Находим вершину с наименьшим расстоянием
-        min_distance = inf
-        current_node = None
-        for node in unvisited_nodes:
-            if distances[node] < min_distance:
-                min_distance = distances[node]
-                current_node = node
+        min_dist = inf
+        curr_node = None
+        for n in unvisited:
+            if dist[n] < min_dist:
+                min_dist = dist[n]
+                curr_node = n
 
         # Удаляем текущую вершину из непосещенных
-        unvisited_nodes.remove(current_node)
+        unvisited.remove(curr_node)
 
         # Если расстояние до текущей вершины равно бесконечности, значит, достигли несвязанных вершин
-        if distances[current_node] == inf:
+        if dist[curr_node] == inf:
             break
 
         # Проходим по соседям текущей вершины
-        for neighbor in graph.neighbors(current_node):
+        for neighbor in graph.neighbors(curr_node):
             # Вычисляем новое расстояние от начальной вершины до соседа
-            distance = distances[current_node] + graph[current_node][neighbor]['weight']
+            d = dist[curr_node] + graph[curr_node][neighbor]['weight']
             # Если новое расстояние меньше текущего, обновляем расстояние и предшественника
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                predecessors[neighbor] = current_node
+            if d < dist[neighbor]:
+                dist[neighbor] = d
+                preds[neighbor] = curr_node
 
-    return distances, predecessors
+    return dist, preds
 
 
 # Пример использования
@@ -50,11 +50,10 @@ if __name__ == "__main__":
     # Создаем граф
     G = nx.Graph()
     G.add_edge('A', 'B', weight=1)
-    G.add_edge('B', 'C', weight=1)
+    G.add_edge('A', 'C', weight=4)
+    G.add_edge('B', 'C', weight=2)
+    G.add_edge('B', 'D', weight=5)
     G.add_edge('C', 'D', weight=1)
-    G.add_edge('D', 'E', weight=5)
-    G.add_edge('D', 'G', weight=3)
-    G.add_edge('C', 'F', weight=3)
 
     # Запускаем алгоритм Дейкстры
     distances, predecessors = dijkstra(G, 'A')
